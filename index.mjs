bc10c7352999102799b34bc124ffeadd411d963d
Sparrow Estes <sparrow.estes1@phoenix.edu>
2022-03-29 15:01:57 -0700

    writes a docker image

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
-import { loadStdlib } from '@reach-sh/stdlib'
+import { loadStdlib } from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs'
 
-const stdlib = loadStdlib(process.env)
+const stdlib = loadStdlib(process.env);
 
 (async ()  => {
     const startingBalance = stdlib.parseCurrency(10)
@@ -9,10 +9,10 @@ const stdlib = loadStdlib(process.env)
    const accGiacomo = await stdlib.newTestAccount(startingBalance)
    const accVera = await stdlib.newTestAccount(startingBalance)
 
-   const ctcGiacomo = accGiacomo.deploy(backend)
-   const ctcVera = accVera.attach(backend, ctcGiacomo.getInfo())
+   const ctcGiacomo = accGiacomo.contract(backend)
+   const ctcVera = accVera.contract(backend, ctcGiacomo.getInfo())
 
-   await Promise.add ([
+   await Promise.all ([
     backend.Giacomo(ctcGiacomo, {
         //implement Giacomo's interact object here
     }),
