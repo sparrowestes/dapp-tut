a329e798b2a2776f5d41ecac9c2c0cc16a216a4e
Sparrow Estes <sparrow.estes1@phoenix.edu>
2022-03-29 16:06:57 -0700

    automates rock paper scissors game

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ import * as backend from './build/index.main.mjs'
 const stdlib = loadStdlib(process.env);
 
 (async ()  => {
-    const startingBalance = stdlib.parseCurrency(10)
+    const startingBalance = stdlib.parseCurrency(100)
 
    const accGiacomo = await stdlib.newTestAccount(startingBalance)
    const accVera = await stdlib.newTestAccount(startingBalance)
@@ -12,13 +12,27 @@ const stdlib = loadStdlib(process.env);
    const ctcGiacomo = accGiacomo.contract(backend)
    const ctcVera = accVera.contract(backend, ctcGiacomo.getInfo())
 
+   const HAND = ['Rock', 'Paper', 'Scissors']
+   const OUTCOME = ['Vera wins', 'Draw', 'Giacomo wins']
+
+   const Player = (who) => ({
+        getHand: () => {
+            const hand = Math.floor(Math.random() * 3)
+            console.log(`${who} played ${HAND[hand]}`)
+            return hand
+        },
+        seeOutcome: (outcome) => {
+            console.log(`${who} saw outcome ${OUTCOME[outcome]}`)
+        }
+   })
+
    await Promise.all ([
     backend.Giacomo(ctcGiacomo, {
-        //implement Giacomo's interact object here
+        ...Player('Giacomo')
     }),
 
     backend.Vera(ctcVera, {
-        //implement Vera's interact object here
+        ...Player('Vera')
     })
 ])
 })();
--- index.rsh
+++ index.rsh
@@ -1,16 +1,38 @@
 'reach 0.1'
 
+const Player = {
+  getHand: Fun([], UInt),
+  seeOutcome: Fun([UInt], Null)
+}
+
 export const main = Reach.App(() => {
 
   const Giacomo = Participant('Giacomo', {
-    // Specialize interact interface here
+    ...Player,
   })
   
   const Vera = Participant('Vera', {
-    // Specialize interact interface here
+    ...Player,
   });
 
   deploy();
 
-  // Write logic
+  Giacomo.only(() => {
+    const handGiacomo = declassify(interact.getHand())
+  })
+  Giacomo.publish(handGiacomo)
+  commit()
+
+  Vera.only(() => {
+    const handVera = declassify(interact.getHand())
+  })
+  Vera.publish(handVera)
+
+  const outcome = (handGiacomo + (4 - handVera)) % 3
+  commit()
+
+  each([Giacomo, Vera], () => {
+    interact.seeOutcome(outcome)
+  })
+
 });
